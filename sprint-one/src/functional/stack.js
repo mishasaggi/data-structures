8ca5be5cabfe12cdf0a48479b4e4813fe6c5d991
mishasaggi <monisha.saggi@gmail.com>
2015-11-04 09:36:29 -0600

    Complete functional pattern instantiation stack.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,25 @@
 var Stack = function(){
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
+  var positionPointer = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[positionPointer] = value;
+    positionPointer++;
   };
 
   someInstance.pop = function(){
+    if (someInstance.size() > 0) {
+      positionPointer--;
+      var result = storage[positionPointer];
+      return result;
+    } else return;
   };
 
   someInstance.size = function(){
+    return positionPointer;
   };
 
   return someInstance;
