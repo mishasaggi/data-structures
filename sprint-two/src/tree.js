287cffcb1fcb78528ecb6f26d6e87243698f25c9
mishasaggi <monisha.saggi@gmail.com>
2015-11-05 10:33:39 -0600

    Tree in progress. Adds children. Contains needs to be fixed.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,25 +1,30 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target){
-
+treeMethods.contains = function(target, currentNode){
+  //recursively search through children arrays
+  currentNode = currentNode || this;
+  if (currentNode.value === target) return true;
+  if (currentNode.children.length > 0) {
+    for (var i = 0; i < currentNode.children.length; i++){
+      if (currentNode.children[i].value === target) return true;
+        else return this.contains(target, currentNode.children[i])
+      }
+    }
+    return false;
 };
 
 
