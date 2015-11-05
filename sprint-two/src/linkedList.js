b06b110321ff0578fddfa04764da2f9d7e52798e
mishasaggi <monisha.saggi@gmail.com>
2015-11-05 09:59:04 -0600

    Complete linked list in functional style.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(list.head === null && list.tail === null) {
+      list.tail = Node(value);
+      list.head = list.tail;
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+    var temp = list.head;
+    delete list.head;
+    list.head = temp.next;
+    return result;
   };
 
   list.contains = function(target){
+    list.search = list.head;
+    if(list.tail.value === target) return true;
+    while (list.search.next !== null) {
+      if(list.search.value === target) return true;
+      list.search = list.search.next;
+     }
+     return false;
   };
 
   return list;
@@ -26,4 +45,7 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addToTail: O(1)
+ * removeHead: O(1)
+ * contains: O(n)
  */
