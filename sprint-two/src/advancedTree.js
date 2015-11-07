dcb8f5063ee719ad06629d2cbe494008c83ce8db
mishasaggi <monisha.saggi@gmail.com>
2015-11-06 23:51:53 -0600

    Add advanced tree specs and create tree with parent pointer and parent remove method.

---
+++ sprint-two/src/advancedTree.js
@@ -0,0 +1,44 @@
+var Tree = function(value, parent){
+  var newTree = {};
+  newTree.value = value;
+  newTree.parent = parent;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
+
+  return newTree;
+};
+
+
+var treeMethods = {};
+
+treeMethods.addChild = function(value){
+  this.children.push(Tree(value, this));
+};
+
+treeMethods.removeFromParent = function(){
+  //disassociates the node from parent 
+  for (var i = 0; i < this.parent.children.length; i++) {
+    if(this.parent.children[i].value === this.value) {
+      this.parent.children[i].value = null;
+    }
+  }
+  this.parent = null;
+  
+};
+
+treeMethods.contains = function(target){
+  //recursively search through children arrays
+  if (this.value === target) return true;
+    for (var i = 0; i < this.children.length; i++){
+      if (this.children[i].contains(target)) return true;
+    }
+    return false;
+};
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ * addChild: O(1)
+ * removeFromParent: O(n)
+ * contains: O(x^n)
+ */
